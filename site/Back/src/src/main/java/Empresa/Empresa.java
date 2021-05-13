80e2ca4a1618b2b92b848a363a32d36c66e48172
Eric <quebec.ericjs@gmail.com>
2021-05-12 23:24:53 -0300

    e foda

--- site/Back/src/src/main/java/Empresa/Empresa.java
+++ site/Back/src/src/main/java/Empresa/Empresa.java
@@ -74,79 +74,8 @@ public class Empresa {
 	@Override
 	public String toString() {
 		return "Empresa [idEmpresa=" + idEmpresa + ", nomeEmpresa=" + nomeEmpresa + ", eMailEmpresa=" + eMailEmpresa
-				+ ", senhaEmpresa=" + senhaEmpresa + "]";
+				+ ", senhaEmpresa=" + senhaEmpresa + ", imagemEmpresa=" + imagemEmpresa + "]";
 	}
-/*
-	public static void main(String[] args) {
-		staticFiles.location("src/main/resources");
-		Teste oia = new Teste();
-		BigProjectA conectar = new BigProjectA();
-		conectar.conectarPost();
-
-		get("/", (req, res) -> oia.renderContent("/home.html")); // funcionando
-		get("/login", (req, res) -> oia.renderContent("/login.html"));// funcionando
-		get("/register", (req, res) -> oia.renderContent("/register.html")); // funcionando
-		get("/myprojects", (req, res) -> oia.renderContent("/myprojects.html")); // funcionando
-		get("/creation", (req, res) -> oia.renderContent("/creation.html")); // funcionando
-		get("/project", (req, res) -> oia.renderContent("/project.html"));// funcionando
-		get("/mycomments", (req, res) -> oia.renderContent("/mycomments.html"));// funcionando
-
-		get("/pesquisa", (req, res) -> {
-			// System.out.println("ola22");
-			String pegar = "";
-			System.out.println(pegar = req.queryParams("query"));
-			return 200;
-		});
-
-		get("/mandarRe", (req, res) -> {
-
-			String nomeUU = "";
-			System.out.println(nomeUU = req.queryParams("query"));
-			String[] realocacao = nomeUU.split("/");
-			Empresa mandarParaPhpAdmin = new Empresa(1, realocacao[0], realocacao[1], realocacao[2]);
-			conectar.inserirEmpresa(mandarParaPhpAdmin);
-			// System.out.println( senhaUU=req.queryParams("query1"));
-			return 200;
-		});
-
-		// String pegarT = oia.getParameter("nome");
-		// System.out.println(doPost(site));
-		Scanner ler = new Scanner(System.in);
-
-		conectar.conectarPost(); // para conectar
-		int x = ler.nextInt();
-		if (x == 1) {
-			Empresa empresaPhpAdmin = conectar.pegarEmpresa();
-			System.out.println(empresaPhpAdmin.toString());
-			fazerteste(empresaPhpAdmin);
-		} else if (x == 2) {
-
-			// conectar.inserirEmpresa(mandarParaPhpAdmin);
-		} else {
-			Empresa empresaPhpAdmin = conectar.pegarEmpresa();
-
-			String receberSenha = "ttt";
-			if (receberSenha == empresaPhpAdmin.getSenhaEmpresa()) {
-				System.out.println("Mesma senha");
-			}
-			String receberEmail = "bruno@gmail.com";
-			if (receberEmail == empresaPhpAdmin.geteMailEmpresa()) {
-				System.out.println("Mesmo email");
-			}
-			Empresa atualizarEmpresa = new Empresa(empresaPhpAdmin.getIdEmpresa(), empresaPhpAdmin.getNomeEmpresa(),
-					receberEmail, receberSenha);
-			conectar.atualizarEmpresa(atualizarEmpresa);
-		}
-		ler.close();
-
-	}
-
-	public static void fazerteste(Empresa empresaPhpAdmin) {
-		System.out.println("ola22");
-		get("/empresa1", (request, response) -> empresaPhpAdmin.toString());
-
-	}
-*/
 }
 
 
--- site/Back/src/src/main/java/app/Aplicacao.java
+++ site/Back/src/src/main/java/app/Aplicacao.java
@@ -3,112 +3,108 @@ package app;
 import static spark.Spark.*;
 
 import java.io.IOException;
+
 import java.net.URISyntaxException;
+
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.sql.Date;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Scanner;
+
 import Classe.BigProjectA;
 import Empresa.Empresa;
 import Pojetos.Projetos;
 import Usuario.Usuario;
-import java.util.Random;
 
 public class Aplicacao {
+	private final static String USER_AGENT = "Mozilla/5.0";
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws Exception {
+
 		staticFiles.location("src/main/resources"); // pegar todos os documentos da pasta reources
-		RetornaroFront mandarSite=new RetornaroFront();// chamar   class que ira mandar nossa pagina html para a tela
-		
+		RetornaroFront mandarSite = new RetornaroFront();// chamar class que ira mandar nossa pagina html para a tela
+
 		BigProjectA conectar = new BigProjectA(); // conectar com nosso banco de dados
 		conectar.conectarPost();
-		
-		/* codigos onde iremos colocar a pagina on*/
+
+		/* codigos onde iremos colocar a pagina on */
 		get("/", (req, res) -> mandarSite.renderContent("/home.html"));
 		get("/login", (req, res) -> mandarSite.renderContent("/login.html"));
-		get("/register", (req, res) -> mandarSite.renderContent("/register.html")); 
-		get("/myprojects", (req, res) -> mandarSite.renderContent("/myprojects.html")); 
-		get("/creation", (req, res) -> mandarSite.renderContent("/creation.html")); 
+		get("/register", (req, res) -> mandarSite.renderContent("/register.html"));
+		get("/myprojects", (req, res) -> mandarSite.renderContent("/myprojects.html"));
+		get("/creation", (req, res) -> mandarSite.renderContent("/creation.html"));
 		get("/project", (req, res) -> mandarSite.renderContent("/project.html"));
 		get("/mycomments", (req, res) -> mandarSite.renderContent("/mycomments.html"));
-		
-		
+
 		// pegar as infomacoes do usuario
 		get("/mandarRe", (req, res) -> {
-			
+
 			String nomeUU = "";
-			
+
 			System.out.println(nomeUU = req.queryParams("query"));
 			String[] realocacao = nomeUU.split(",");
-			int x= Integer.parseInt(realocacao[5]);
-			Usuario alocar = new Usuario(x, realocacao[0],realocacao[1],realocacao[2],realocacao[3],realocacao[4],0);
+			int x = Integer.parseInt(realocacao[5]);
+			Usuario alocar = new Usuario(x, realocacao[0], realocacao[1], realocacao[2], realocacao[3], realocacao[4],
+					0);
 			conectar.inserirUsuario(alocar);
 			return 200;
 		});
-		
+
 		// pegar empresa
 		get("/empresaRe", (req, res) -> {
 
 			String nomeUU = "";
 			System.out.println(nomeUU = req.queryParams("query"));
 			String[] realocacao = nomeUU.split(",");
-			int x= Integer.parseInt(realocacao[4]);
-			Empresa mandarParaPhpAdmin = new Empresa(x, realocacao[0], realocacao[1], realocacao[2],realocacao[3]);
+			int x = Integer.parseInt(realocacao[4]);
+			Empresa mandarParaPhpAdmin = new Empresa(x, realocacao[0], realocacao[1], realocacao[2], realocacao[3]);
 			conectar.inserirEmpresa(mandarParaPhpAdmin);
 			// System.out.println( senhaUU=req.queryParams("query1"));
 			return 200;
 		});
-		
+
 		// pegar projetos
-		get("/projetoRe", (req,res) ->{
-			String getProjeto="";
+		get("/projetoRe", (req, res) -> {
+			String getProjeto = "";
 			LocalDate date = LocalDate.now(); // Gets the current date
-			
-		
-			
+
 			System.out.println(getProjeto = req.queryParams("query"));
-			String [] realocarProjeto=getProjeto.split("//");
-			int idProjeto= Integer.parseInt(realocarProjeto[6]);
-			int custoProjeto= Integer.parseInt(realocarProjeto[2]);
-			int dataAsermodificada= Integer.parseInt(realocarProjeto[1]);
+			String[] realocarProjeto = getProjeto.split("//");
+			int idProjeto = Integer.parseInt(realocarProjeto[6]);
+			int custoProjeto = Integer.parseInt(realocarProjeto[2]);
+			int dataAsermodificada = Integer.parseInt(realocarProjeto[1]);
 			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-			//System.out.println(date.format(formatter));
-			String pegarInicio=date.format(formatter);
-			date=date.plusDays(dataAsermodificada);
-			String pegarFim=date.format(formatter);
-			Projetos projeto = new Projetos(idProjeto,realocarProjeto[0],realocarProjeto[4],pegarInicio,pegarFim
-						,custoProjeto,realocarProjeto[3],realocarProjeto[7],realocarProjeto[5]);
-			conectar.inserirProjeto(projeto);
-			
-	
+			// System.out.println(date.format(formatter));
+			String pegarInicio = date.format(formatter);
+			date = date.plusDays(dataAsermodificada);
+			String pegarFim = date.format(formatter);
+			Projetos projeto = new Projetos(idProjeto, realocarProjeto[0], realocarProjeto[4], pegarInicio, pegarFim,
+					custoProjeto, realocarProjeto[3], realocarProjeto[7], realocarProjeto[5]);
+
+			 conectar.inserirProjeto(projeto);
+
 			return 200;
 		});
-		
-		//pegar os comentarios do projeto
-		get("/comentarioGet", (req,res)->{
-			String getComentario="";
+
+		// pegar os comentarios do projeto
+		get("/comentarioGet", (req, res) -> {
+			String getComentario = "";
 			System.out.println(getComentario = req.queryParams("query"));
-			String [] realocarProjeto=getComentario.split(",");
-			
-			
+			String[] realocarProjeto = getComentario.split(",");
+
 			return 200;
 		});
-		
-		
-			
 
 	}
+
 }
+
 // class para usar a funcao sem static
 class RetornaroFront {
 
-	 // String renderContent fuciona para colocar os html na pagina 
+	// String renderContent fuciona para colocar os html na pagina
 	public String renderContent(String htmlFile) throws IOException, URISyntaxException {
 		return new String(Files.readAllBytes(Paths.get(getClass().getResource(htmlFile).toURI())),
 				StandardCharsets.UTF_8);
